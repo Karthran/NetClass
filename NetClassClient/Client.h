3e2dcb67615147786c988ed4ed514e5a7838ce81
Karthran <rksm29@rambler.ru>
2021-12-18 14:10:35 +0300

    fix add thread to class

--- NetClassClient/Client.h
+++ NetClassClient/Client.h
@@ -1,16 +1,29 @@
 #pragma once
 #include <string>
+
+const int DEFAULT_BUFLEN = 1024;
+
 class Client
 {
 public:
 	auto run() -> void;
     auto getOutMessageReady()const -> bool;
-    auto setOutMessageReady(bool flag)const -> void;
+    auto setOutMessageReady(bool flag) -> void;
     auto getInMessageReady()const -> bool;
-    auto setInMessageReady(bool flag)const -> void;
+    auto setInMessageReady(bool flag) -> void;
     auto getMessage() -> const std::string&;
-    auto setMessage(const std::string& msg)const -> void;
+    auto setMessage(const std::string& msg) -> void;
     auto getServerError()const -> bool;
-    auto setBufferSize(size_t size) const -> void;
+    auto setBufferSize(size_t size)  -> void;
+
+private:
+    volatile bool out_message_ready{false};
+    volatile bool in_message_ready{false};
+    volatile bool server_error{false};
+    std::string message{};
+    volatile size_t buffer_size{DEFAULT_BUFLEN};
+    volatile bool need_buffer_resize{true};
+
+    auto client_thread() -> int;
 };
 
